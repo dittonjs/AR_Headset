f5d70f4eb81cafca9224b92200fffa0420f59af5
dittonjs <jditton.atomic@gmail.com>
2016-10-07 15:58:26 -0600

    The beginning of a beautiful thing

---
+++ Assets/scripts/TimeScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+public class TimeScript : MonoBehaviour {
+	public Text time;
+	void Update () {
+		time.text = DateTime.Now.ToLongTimeString();
+	}
+}
---
+++ Assets/scripts/WeatherScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WeatherScript : MonoBehaviour {
+	public Text weatherText;
+
+	void Awake () {
+		Debug.Log ("HERE");
+		string url = "api.openweathermap.org/data/2.5/weather?zip=84321&APPID=4eac3f75191b45b083f1b27d84ba634a";
+		WWW www = new WWW(url);
+		StartCoroutine(WaitForRequest(www));
+	}
+
+	IEnumerator WaitForRequest(WWW www)
+	{
+		yield return www;
+
+		// check for errors
+		if (www.error == null)
+		{
+			JSONObject data = new JSONObject (www.text);
+			Debug.Log (data);
+			string weather = data.GetField ("weather") [0].GetField ("main").ToString();
+			string temperature = data.GetField("main").GetField("temp").ToString();
+			weatherText.text = weather + " | " + temperature;
+
+		} else {
+			Debug.Log("WWW Error: "+ www.error);
+		}    
+	}
+}
+	
+
