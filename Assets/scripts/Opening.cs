1f337da6ca5688c569a0a549d77e09d8bf96eb93
dittonjs <jditton.atomic@gmail.com>
2016-10-08 15:02:50 -0600

    more stuff

---
+++ Assets/scripts/Opening.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Windows;
+public class Opening : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		TextToSpeech.SpeakText ("Welcome to Smart Life Augmented Reality.");
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Application.LoadLevel (1);
+		}
+	}
+}	
---
+++ Assets/scripts/TextToSpeech.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+
+public class TextToSpeech
+{
+	public static void SpeakText(string s)
+	{
+		string file = "/Users/josephditton/Desktop/text.txt";
+		File.WriteAllText(file, s);
+		Process p = new Process();
+		p.StartInfo.UseShellExecute = false;
+		p.StartInfo.RedirectStandardInput = true;
+		p.StartInfo.RedirectStandardOutput = true;
+		p.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+		p.StartInfo.FileName ="open";
+		p.StartInfo.Arguments = "Talk.command";
+		p.StartInfo.WorkingDirectory="/Users/josephditton/Desktop";
+		p.StartInfo.CreateNoWindow = false;
+		p.Start();
+	}
+}
