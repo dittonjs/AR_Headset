435b3198a0b22785a693c8ed3f69987f32d3e6dc
dittonjs <jditton.atomic@gmail.com>
2016-10-07 19:55:01 -0600

    adds sound recording

---
+++ Assets/scripts/MicInput.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MicInput : MonoBehaviour {
+
+	void Start() {
+		StartCoroutine (GetMicInput());
+	}
+
+	IEnumerator GetMicInput(){
+		AudioSource aud = GetComponent<AudioSource>();
+		aud.clip = Microphone.Start("External Microphone", false, 10, 44100);
+		yield return new WaitForSeconds (10);
+		Microphone.End("External Microphone");
+		aud.Play();	
+	}
+}
