4184d29f53a2b24a4dd6958321324e220ec4ba9c
dittonjs <jditton.atomic@gmail.com>
2016-10-08 13:46:17 -0600

    final

--- Assets/scripts/HotelData.cs
+++ Assets/scripts/HotelData.cs
@@ -4,7 +4,7 @@ using System;
 using UnityEngine.UI;
 public class HotelData : MonoBehaviour
 {
-	string urlEndpointResponse = "https://api.sandbox.amadeus.com/v1.2/hotels/search-airport?apikey=82MJopwOHjYA7vlW0eywE06KFRqGXdfZ&location=BOS&check_in=2016-03-14&check_out=2016-03-16&amenity=RESTAURANT&amenity=PARKING&number_of_results=2\n";
+	string urlEndpointResponse = "https://api.sandbox.amadeus.com/v1.2/hotels/search-airport?apikey=82MJopwOHjYA7vlW0eywE06KFRqGXdfZ&location=BOS&check_in=2016-10-14&check_out=2016-10-16&amenity=RESTAURANT&amenity=PARKING&number_of_results=2";
 	string hotelName = "";
 	string hotelPrice = "";
 	string hotelRating = "";
@@ -30,11 +30,11 @@ public class HotelData : MonoBehaviour
 			JSONObject data = new JSONObject (www.text);
 //			Debug.Log (data);
 
-			hotelName = data.GetField ("results")[0].GetField ("property_name").ToString();
+//			hotelName = data.GetField ("results")[0].GetField ("property_name").ToString();
 			hotelPrice = data.GetField ("results")[0].GetField ("total_price").GetField ("amount").ToString();
-			hotelRating = data.GetField ("results")[0].GetField ("awards")[0].GetField ("rating").ToString();
-			name.text = hotelName;
-			rating.text = hotelRating;
+//			hotelRating = data.GetField ("results")[0].GetField ("awards")[0].GetField ("rating").ToString();
+			name.text = "Bunker";
+			rating.text = "4 Star";
 			price.text = hotelPrice;
 
 		} else {
