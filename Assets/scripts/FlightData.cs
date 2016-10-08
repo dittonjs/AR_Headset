0790b142f586a1d2d27a5ecebda46b1528433701
Benjamin Call <llacneb@gmail.com>
2016-10-08 11:31:05 -0600

    uses airline api to get info

---
+++ Assets/scripts/FlightData.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlightData : MonoBehaviour
+{
+	string urlEndpointResponse = "http://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?origin=IST&destination=BOS&departure_date=2016-10-15&return_date=2016-10-21&number_of_results=3&apikey=82MJopwOHjYA7vlW0eywE06KFRqGXdfZ";
+
+	// Use this for initialization
+	void Start ()
+	{
+		WWW www = new WWW(urlEndpointResponse);
+		StartCoroutine(WaitForRequest(www));
+	}
+
+	IEnumerator WaitForRequest(WWW www)
+	{
+		yield return www;
+
+		// check for errors
+		if (www.error == null)
+		{
+			Debug.Log ("asdf" + www.text);
+			JSONObject data = new JSONObject (www.text);
+			Debug.Log (data);
+//			string weather = data.GetField ("weather") [0].GetField ("main").ToString();
+//			string temperature = data.GetField("main").GetField("temp").ToString();
+//			weatherText.text = weather + " | " + temperature;
+
+		} else {
+			Debug.Log("WWW Error: "+ www.error);
+		}    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
--- Assets/scripts/MicInput.cs
+++ Assets/scripts/MicInput.cs
@@ -1,30 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System;
-using System.Text;
+
+
 public class MicInput : MonoBehaviour {
-//	AudioClip clip;
-//	void Start() {
-//		StartCoroutine (GetMicInput());
-//	}
-//
-//	IEnumerator GetMicInput(){
-//		Debug.Log ("RECORDING");
-//		AudioSource aud = GetComponent<AudioSource>();
+	AudioClip clip;
+	void Start() {
+		StartCoroutine (GetMicInput());
+	}
+
+	IEnumerator GetMicInput(){
+		//Debug.Log ("RECORDING");
+		AudioSource aud = GetComponent<AudioSource>();
 //		clip = Microphone.Start("External Microphone", false, 3, 44100);
 //		yield return new WaitForSeconds (3);
 //		Microphone.End("External Microphone");
-//		Debug.Log ("DONE RECORDING");
-//		SavWav.Save ("myvoice", clip);
+
+//		SavWav.Save ("myvoice1", clip);
 //		yield return new WaitForSeconds (2);
-//		string url = Application.persistentDataPath + "/myvoice.wav";
-//		Debug.Log (url);
-//		WWW audioloader = new WWW ("file:///"+url);
-//		Debug.Log ("SAVING");
-//		byte[] bytes = Encoding.ASCII.GetBytes (audioloader.text);
-//		string encodedText = Convert.ToBase64String (bytes);
-//		yield return audioloader;
-//		aud.clip = audioloader.GetAudioClip (false, false, AudioType.WAV);
+		string url = Application.persistentDataPath + "/myvoice.wav";
+//		Convert.ToBase64String(byte[] data)
+//		Debug.Log (url.ToString());
+		WWW audioloader = new WWW ("file:///" + url);
+//
+		yield return audioloader;
+		Debug.Log (audioloader.text);
+
+//		var asdf = Convert.ToBase64String(byte[] aud.clip);  
+
+		aud.clip = audioloader.GetAudioClip(false, false, AudioType.WAV);
+		aud.Play ();
+//
+
+
 //		aud.Play();	
-//	}
+	}
 }
