e82051f6c62ca2c800413877a3944e8fcdbdee25
dittonjs <jditton.atomic@gmail.com>
2016-10-07 20:48:29 -0600

    whatever

--- Assets/scripts/MicInput.cs
+++ Assets/scripts/MicInput.cs
@@ -2,16 +2,27 @@
 using System.Collections;
 
 public class MicInput : MonoBehaviour {
-
+	AudioClip clip;
 	void Start() {
 		StartCoroutine (GetMicInput());
 	}
 
 	IEnumerator GetMicInput(){
+		Debug.Log ("RECORDING");
 		AudioSource aud = GetComponent<AudioSource>();
-		aud.clip = Microphone.Start("External Microphone", false, 10, 44100);
-		yield return new WaitForSeconds (10);
+		clip = Microphone.Start("External Microphone", false, 3, 44100);
+		yield return new WaitForSeconds (3);
 		Microphone.End("External Microphone");
-		aud.Play();	
+		Debug.Log ("DONE RECORDING");
+		SavWav.Save ("myvoice", clip);
+		yield return new WaitForSeconds (2);
+		string url = Application.persistentDataPath + "/myvoice.wav";
+		Debug.Log (url);
+		WWW audioloader = new WWW (url);
+		Debug.Log ("SAVING");
+		yield return audioloader;
+		Debug.Log(audioloader.audioClip.name);
+//		Debug.Log("did i break?");
+//		aud.Play();	
 	}
 }
